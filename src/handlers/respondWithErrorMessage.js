f08190d11e98cf43d955b8e3a37ffcc0e5ce4b70
Rishav Sharan <rishav.sharan@live.com>
2023-08-11 15:32:07 +0530

    clean start

--- src/handlers/respondWithErrorMessage.js
+++
@@ -1,9 +0,0 @@
-export const respondWithErrorMessage = async (e) => {
-
-    let headers = new Headers(
-            {'content-type': 'application/json;charset=UTF-8'},
-            {'Powered-by': 'API: Pika Pika Pika Choooo'}
-    )
-
-    return new Response(JSON.stringify({"error": e.message}), { status: e.message, headers: headers })
-}
--- src/views/postCard.js
+++
@@ -1,7 +0,0 @@
-export const postCard = (post) => {   
-    return /*html*/`
-    <li> <a href="/p/${post.id}">${post.title}</a></li>
-    `
-}
-
-
