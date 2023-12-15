602cee250e0efaab93d97e46cf4319ff76f77137
Rishav Sharan <rishav.sharan@live.com>
2023-12-15 11:40:17 +0530

    fixed issue with overflow on mobile

--- src/views/postCard.ts
+++ src/views/postCard.ts
@@ -21,7 +21,7 @@ export const PostCard = (post: Post) => {
 
             </button>
 
-            <button class="btn  btn-sm lg:btn-md btn-primary rounded-br-none rounded-tl-none "> 
+            <button class="btn btn-sm lg:btn-md rounded-br-none rounded-tl-none "> 
                 <div class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden lg:block w-4 h-4">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
@@ -66,7 +66,7 @@ export const PostCard = (post: Post) => {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
                                 </svg>
                             </span>
-                            <span class="text-xs"> 12 hours ago </span>
+                            <span class="text-xs"> 12 hours </span>
 
                         </div>
                         <div class="flex lg:gap-1">
@@ -83,7 +83,7 @@ export const PostCard = (post: Post) => {
                             </button>
                                         
                             <button class="btn btn-sm lg:btn-md btn-error rounded-none rounded-br-md w-16 lg:w-36 p-0 text-neutral-content">
-                                <span>9999</span>
+                                <span>9.9k</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
                                 </svg>
